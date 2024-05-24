ba3e6637d1b4103a97f5c8c5e104a93a38b89eac
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-05-24 18:23:18 +0700

    Tests

---
+++ He/Basic/HeButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Basic;
+
+public class HeButton : Control {
+
+    #region DependencyProperties
+
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+        nameof(CornerRadius),
+        typeof(CornerRadius),
+        typeof(HeButton),
+        new PropertyMetadata(new CornerRadius(2)));
+
+    #endregion
+
+    #region Properties
+
+    public CornerRadius CornerRadius {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+
+    #endregion
+    
+    static HeButton() {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(HeButton),
+            new FrameworkPropertyMetadata(typeof(HeButton)));
+    }
+}
