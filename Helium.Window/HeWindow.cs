551382d3a62c51acf3f713749742256ec90a7d7a
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-06-10 17:25:18 +0700

    HELIUM-4: Создан проект для реализации кастомного окна

---
+++ Helium.Window/HeWindow.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Helium.Window;
+
+public class HeWindow : System.Windows.Window {
+    static HeWindow() {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(HeWindow),
+            new FrameworkPropertyMetadata(typeof(HeWindow)));
+    }
+}
