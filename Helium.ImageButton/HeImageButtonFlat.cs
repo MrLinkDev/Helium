a7a96e0d9f6fb040c35854e1828ca63464017384
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-09-03 11:17:46 +0700

    Добавлен ImageButton в плоском варианте

---
+++ Helium.ImageButton/HeImageButtonFlat.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Helium.ImageButton;
+
+public class HeImageButtonFlat : HeImageButtonBase {
+    
+    static HeImageButtonFlat() {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(HeImageButtonFlat),
+            new FrameworkPropertyMetadata(typeof(HeImageButtonFlat)));
+    }
+}
