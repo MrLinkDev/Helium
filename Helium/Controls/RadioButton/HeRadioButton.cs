867a42049b99ec98760aff6ceb89af43505658bc
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-09-04 17:52:21 +0700

    Исправлена проблема с нерабочим тулбаром, если в нём нет элементов

---
+++ Helium/Controls/RadioButton/HeRadioButton.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Helium.Controls.RadioButton;
+
+public class HeRadioButton : System.Windows.Controls.RadioButton {
+    static HeRadioButton() {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(HeRadioButton),
+            new FrameworkPropertyMetadata(typeof(HeRadioButton)));
+    }
+}
