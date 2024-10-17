549e05c8d9cd84e8ea4b2794515d0ffda06a4270
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-10-17 12:25:30 +0700

    Добавлен фиксированный отступ у сетки

--- Helium.Demo/OpenGLDemo.xaml.cs
+++ Helium.Demo/OpenGLDemo.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using Helium.Controls.Window;
 using Helium.Plot;
@@ -13,6 +14,8 @@ public partial class OpenGLDemo : HeWindow {
     
     private float[] data1;
     private float[] data2;
+    private float[] data3;
+    private float[] data4;
 
     private const float f = 2;
     private float c = 2 * Single.Pi * f;
@@ -23,40 +26,71 @@ public partial class OpenGLDemo : HeWindow {
     private uint markerId = 0;
     private float markerPos = -1f + ((float)2 / (points - 1));
 
-    private float minPos = 2e9f;
-    private float maxPos = 5e9f;
+    private float minPosX = -5e9f;
+    private float maxPosX = -2e9f;
+    
+    private float minPosY = 2e9f;
+    private float maxPosY = 5e9f;
 
     private float[] markersPos = new float[10];
-
+    
     public OpenGLDemo() {
         InitializeComponent();
 
         Plot.OnReady = () => {
             data1 = new float[size];
             data2 = new float[size];
+            data3 = new float[4];
+            data4 = new float[4];
             
             GCHandle data1Handle = GCHandle.Alloc(data1, GCHandleType.Pinned);
             GCHandle data2Handle = GCHandle.Alloc(data2, GCHandleType.Pinned);
+            GCHandle data3Handle = GCHandle.Alloc(data3, GCHandleType.Pinned);
+            GCHandle data4Handle = GCHandle.Alloc(data4, GCHandleType.Pinned);
 
             IntPtr data1Ptr = data1Handle.AddrOfPinnedObject();
             IntPtr data2Ptr = data2Handle.AddrOfPinnedObject();
+            IntPtr data3Ptr = data3Handle.AddrOfPinnedObject();
+            IntPtr data4Ptr = data4Handle.AddrOfPinnedObject();
         
             Plot.AddTrace(0, data1Ptr, points);
             Plot.AddTrace(1, data2Ptr, points);
+            Plot.AddTrace(2, data3Ptr, 2);
+            Plot.AddTrace(3, data4Ptr, 2);
         
-            AmethystApi.SetStartStopX(Plot.ScreenPtr, 0, minPos, maxPos);
-            AmethystApi.SetStartStopY(Plot.ScreenPtr, 0, -1, 1);
+            AmethystApi.SetStartStopX(Plot.ScreenPtr, 0, minPosX, maxPosX);
+            AmethystApi.SetStartStopY(Plot.ScreenPtr, 0, minPosY, maxPosY);
         
             AmethystApi.SetStartStopX(Plot.ScreenPtr, 1, -1, 1);
             AmethystApi.SetStartStopY(Plot.ScreenPtr, 1, -1, 1);
+        
+            AmethystApi.SetStartStopX(Plot.ScreenPtr, 2, minPosX, maxPosX);
+            AmethystApi.SetStartStopY(Plot.ScreenPtr, 2, -1, 1);
+            
+            AmethystApi.SetStartStopX(Plot.ScreenPtr, 3, -1, 1);
+            AmethystApi.SetStartStopY(Plot.ScreenPtr, 3, minPosY, maxPosY);
+            
+            data3[0] = minPosX;
+            data3[1] = 0;
+            data3[2] = maxPosX;
+            data3[3] = 0;
+                
+            data4[0] = 0;
+            data4[1] = minPosY;
+            data4[2] = 0;
+            data4[3] = maxPosY;
 
             Plot.OnUpdate = () => {
+                float[] x = new float[points];
+                
                 for (int i = 0; i < points; ++i) {
-                    data1[i * 2 + 0] = minPos + i * ((maxPos - minPos) / (points - 1));
-                    data1[i * 2 + 1] = float.Sin(c * ((float) i / size) + p);
+                    data1[i * 2 + 0] = minPosX + i * ((maxPosX - minPosX) / (points - 1));
+                    data1[i * 2 + 1] =  ((maxPosY + minPosY) / 2) + float.Sin(c * ((float) i / size) + p) * ((maxPosY - minPosY) / 2);
                 
                     data2[i * 2 + 0] = -1.0f + i * ((float)2 / (points - 1));
                     data2[i * 2 + 1] = float.Sin(c * ((float) i / size) + p + d);
+
+                    x[i] = data1[i * 2 + 1];
 
                     if (i == 25) {
                         //Plot.SetMarkerY(0, 0,  data1[i * 2 + 0],  data1[i * 2 + 1]);
