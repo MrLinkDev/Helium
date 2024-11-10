263800e720f44b8e67b1e1d8ae82fb87418f4bc8
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-11-10 21:15:25 +0700

    HLM-23: Создан класс HeComboBox и стили для него

---
+++ Helium/Controls/ComboBox/HeComboBoxItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Helium.Controls.ComboBox;
+
+public class HeComboBoxItem : ComboBoxItem {
+    static HeComboBoxItem() {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(HeComboBoxItem),
+            new FrameworkPropertyMetadata(typeof(HeComboBoxItem)));
+    }
+}
