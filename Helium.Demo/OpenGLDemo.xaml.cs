63aad654e0af3843dff3639600912f9a2cb17393
Alexander Gorbunov <sasha2000.gorbunov@gmail.com>
2024-12-26 23:20:48 +0500

    Pre-Release: v0.5

--- Helium.Demo/OpenGLDemo.xaml.cs
+++ Helium.Demo/OpenGLDemo.xaml.cs
@@ -3,7 +3,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Helium.Controls.Window;
-using Helium.Plot;
 
 namespace HeliumDemo;
 
---
+++ Helium/Controls/Plot/AmethystApi.cs
@@ -0,0 +1,61 @@
+using System.Runtime.InteropServices;
+
+namespace Helium.Controls.Plot;
+
+internal static class AmethystApi {
+    private const string DLL_NAME = "Alexander.Gorbunov.Amethyst.dll";
+    
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr GetTraceContainerPtr(IntPtr screenPtr);
+    
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr SetScreenUpdated(IntPtr screenPtr);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr GetTracePtr(IntPtr containerPtr, int traceId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern IntPtr GetSelectedTracePtr(IntPtr containerPtr);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void AddTrace(IntPtr containerPtr, int traceId, IntPtr dataPtr, uint points);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SelectTrace(IntPtr containerPtr, int traceId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RemoveTrace(IntPtr containerPtr, int traceId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SetData(IntPtr containerPtr, int traceId, IntPtr dataPtr, uint points);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SetStartStopX(IntPtr container, int traceId, float startX, float stopX);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SetStartStopY(IntPtr container, int traceId, float startY, float stopY);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void GetMarkerPtr(IntPtr containerPtr, int traceId, int markerId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void GetSelectedMarkerPtr(IntPtr containerPtr, int traceId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void AddMarker(IntPtr containerPtr, int traceId, int markerId, float x);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SelectMarker(IntPtr containerPtr, int traceId, int markerId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void RemoveMarker(IntPtr containerPtr, int traceId, int markerId);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void PlaceMarker(IntPtr containerPtr, int traceId, int markerId, float x, float y);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SetMarkerX(IntPtr containerPtr, int traceId, int markerId, float x);
+
+    [DllImport(DLL_NAME, CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void SetMarkerY(IntPtr containerPtr, int traceId, int markerId, float y);
+}
